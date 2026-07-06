27f48d96ccd073de20cde1284a4906624300486f
DATBOI-MAYANK <mayank106106@gmail.com>
2026-07-06 19:46:22 +0530

    feat: integrate axios and react-query for weather data fetching, update SearchBox component styling

--- app/components/SearchBox.tsx
+++ app/components/SearchBox.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import { IoSearch } from 'react-icons/io5'
-import { cn } from '../utils/cn';
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+import { cn } from "../utils/cn";
 
 type Props = {
   className?: string;
-  value:string;
-  onChange:React.ChangeEventHandler<HTMLInputElement, HTMLInputElement> | undefined ;
-  onSubmit : React.SubmitEventHandler<HTMLFormElement> | undefined
-}
+  value: string;
+  onChange:
+    | React.ChangeEventHandler<HTMLInputElement, HTMLInputElement>
+    | undefined;
+  onSubmit: React.SubmitEventHandler<HTMLFormElement> | undefined;
+};
 
 function SearchBox(props: Props) {
   return (
-    <form onSubmit={props.onSubmit} className={cn('flex relative items-center justify-center h-10', props.className)}>
-        
-        <input type='text' value={props.value} placeholder='Search location...' onChange={props.onChange} className='px-4 py-2 w-57.5 border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full '  />
+    <form
+      onSubmit={props.onSubmit}
+      className={cn(
+        "flex relative items-center justify-center h-10",
+        props.className,
+      )}
+    >
+      <input
+        type="text"
+        value={props.value}
+        placeholder="Search location..."
+        onChange={props.onChange}
+        className="px-4 py-2 w-57.5 border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full "
+      />
 
-        <button className='px-4 py-2.25 bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-500 whitespace-nowrap h-full'>
-            <IoSearch />
-        </button>
+      <button className="px-4 py-2.25 bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-500 whitespace-nowrap h-full">
+        <IoSearch />
+      </button>
     </form>
-  )
+  );
 }
 
-export default SearchBox
+export default SearchBox;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,111 @@
-import Image from "next/image";
+'use client'
+import axios from "axios";
 import Navbar from "./components/Navbar";
+import { useQuery } from "@tanstack/react-query";
+
+export interface WeatherForecastResponse {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: ForecastItem[];
+  city: City;
+}
+
+export interface ForecastItem {
+  dt: number;
+  main: MainWeather;
+  weather: WeatherCondition[];
+  clouds: Clouds;
+  wind: Wind;
+  visibility: number;
+  pop: number; // probability of precipitation
+  rain?: Rain;
+  sys: ForecastSys;
+  dt_txt: string;
+}
+
+export interface MainWeather {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  sea_level: number;
+  grnd_level: number;
+  humidity: number;
+  temp_kf: number;
+  dew_point?: number;
+}
+
+export interface WeatherCondition {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface Clouds {
+  all: number;
+}
+
+export interface Wind {
+  speed: number;
+  deg: number;
+  gust?: number;
+}
+
+export interface Rain {
+  "3h": number;
+}
+
+export interface ForecastSys {
+  pod: "d" | "n";
+}
+
+export interface City {
+  id: number;
+  name: string;
+  coord: Coord;
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
+export interface Coord {
+  lat: number;
+  lon: number;
+}
 
 export default function Home() {
+
+   const { isLoading, error, data } = useQuery<WeatherForecastResponse>({
+    queryKey: ['repoData'],
+    queryFn: async () =>
+    {
+      const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=noida&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&cnt=56`);
+
+      return data
+    }
+      // fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+      //   res.json(),
+      // ),
+  })
+
+  console.log("Data" , data)
+
+  if (isLoading) return (
+    <div className="flex items-center justify-center min-h-screen ">
+
+      <p className="animate-bounce">
+    Loading...
+      </p>
+
+    </div>
+  )
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
     <div className="flex flex-col gap-4 min-h-screen bg-gray-600">
       <Navbar />
